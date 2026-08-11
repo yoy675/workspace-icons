0ae26f4059b06bd03cd8c9275ec4cedca7d41a84
yoy675 <darobins@g.jct.ac.il>
2026-08-11 17:17:31 +0300

    modified:   install.sh 	modified:   workspaces.desktop

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-mv ~/Desktop/* ./workspaces0/
+mv ~/Desktop/* ./workspace0/
 echo "How many workspaces do you have?"
 read a
+mv ./workspace0 ~/Desktop/
 for (( i=1; i<=$a; i++)); do
 	cp -r ~/Desktop/workspace0/ ~/Desktop/workspace$i/
 done
-mv ./workspaces. ~/Desktop/
 mv ./workspaces.bash ~/.local/bin/
-sed -i 's|\$path|$HOME/.local/bin/workspaces.bash' ./workspaces.desktop
+sed -i 's|\$path|$HOME/.local/bin' ./workspaces.desktop
 mv ./workspaces.desktop ~/.config/autostart/
